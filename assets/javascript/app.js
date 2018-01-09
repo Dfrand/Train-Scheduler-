c7eac813e282c6047000683e348e31bde72fd61f
derek <d.frand@yahoo.com>
2018-01-09 15:42:45 -0600

    basic html added

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+// Firebase key
+// var config = {
+//         apiKey: "AIzaSyDijFIuerjSpGHedmrt6wncFgjw4jbKMas",
+//         authDomain: "test-project-88293.firebaseapp.com",
+//         databaseURL: "https://test-project-88293.firebaseio.com",
+//         projectId: "test-project-88293",
+//         storageBucket: "test-project-88293.appspot.com",
+//         messagingSenderId: "658311507950"
+//     };
+//     firebase.initializeApp(config);
